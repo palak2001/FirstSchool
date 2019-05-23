7c670b984d5561fcb0507f4efdc75add7aa24587
palak2001 <44226699+palak2001@users.noreply.github.com>
2019-05-23 14:11:11 +0530

    Add files via upload

---
+++ ABCD.js
@@ -0,0 +1,16 @@
+window.addEventListener('keydown',play);
+
+function play(e)
+{
+    const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key= document.querySelector(`div[data-key="${e.keyCode}"]`);
+    console.log(key);
+    if(!audio)
+    return;
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add('highlight');
+}
+
+const keys=document.querySelector('.key');
+keys.classList.remove("highlight");
